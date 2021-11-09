9a6e1cedb44bd9969d1e5b671ad66bd497662e06
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-11-09 23:45:01 +0600

    Initial routers set pages set ready to start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route exact path="/home">
+            <Home></Home>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }

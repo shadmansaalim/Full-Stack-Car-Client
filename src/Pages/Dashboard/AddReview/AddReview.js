515f1f5ff08632d3f998611d216a57fa8d7fbb53
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-11-11 20:10:08 +0600

    Review Submitted to database

--- src/Pages/Dashboard/AddReview/AddReview.js
+++ src/Pages/Dashboard/AddReview/AddReview.js
@@ -1,9 +1,72 @@
 import React from 'react';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import swal from 'sweetalert';
+import useAuth from '../../../hooks/useAuth';
+import Rating from 'react-rating';
+import './AddReview.css';
 
 const AddReview = () => {
+    const { user } = useAuth();
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const [value, setValue] = useState(0);
+
+    const onSubmit = data => {
+        data.photoURL = user.photoURL;
+        data.rating = value;
+        fetch('http://localhost:5000/reviews', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                if (result.insertedId) {
+                    swal("Thank you for your feedback", "We have added your feedback on our landing page", "success");
+                    reset();
+                }
+            })
+
+    }
+
     return (
         <div>
-            <h2>Add Review</h2>
+            <h4 className="text-start bg-dark text-white p-3 rounded-3 mt-3">Add A Review To Car Sales</h4>
+            <div className="col-lg-8">
+                <div className="card-body mx-md-4 text-start">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="shadow-lg p-4 mt-4 rounded-3">
+                            <p>Please enter your feedback here</p>
+                            <div className="form-floating mb-3">
+                                <input defaultValue={user?.displayName} type="text" className="form-control" id="floatingServiceName" placeholder="name@example.com" required  {...register("name")} />
+                                <label htmlFor="floatingServiceName"><small>Your Name</small></label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <textarea class="form-control" placeholder="Leave a comment here" id="floatingServiceDescription" style={{ height: '100px' }} {...register("feedback")}></textarea>
+                                <label htmlFor="floatingServiceDescription">Feedback</label>
+                            </div>
+                            <div className="text-start">
+                                <Rating
+                                    className="fs-3"
+                                    emptySymbol="far fa-star icon-color"
+                                    fullSymbol="fas fa-star icon-color"
+                                    fractions={2}
+                                    value={value}
+                                    onChange={(value) => {
+                                        setValue(value);
+                                    }}
+                                ></Rating>
+                            </div>
+                        </div>
+                        <div className="text-start mt-4">
+                            <button className="btn app-main-btn text-white w-100 mb-3" type="submit">Submit</button>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
         </div>
     );
 };

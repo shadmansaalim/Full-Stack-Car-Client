eb03022bed0d8c1d471cf7f41ac4fe8bb6c42072
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-11-28 15:56:58 +0600

    Unecessary files removed

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Navigation from '../../Shared/Navigation/Navigation';
-import FeaturedCars from '../FeaturedCars/FeaturedCars';
 import HomeBanner from '../HomeBanner/HomeBanner';
 import { Container } from 'react-bootstrap';
 import Footer from '../../Shared/Footer/Footer';
@@ -12,21 +11,7 @@ import UsedCars from '../UsedCars/UsedCars';
 import SellCar from '../SellCar/SellCar';
 
 const Home = () => {
-    const [featuredCars, setFeaturedCars] = useState([]);
-    //Cars to display in landing page
-    const size = 6;
     const [reviews, setReviews] = useState([]);
-
-
-    useEffect(() => {
-        fetch(`http://localhost:5000/cars?size=${size}`)
-            .then(res => res.json())
-            .then(cars => setFeaturedCars(cars))
-
-    }, [])
-
-
-
     useEffect(() => {
         fetch('http://localhost:5000/reviews')
             .then(res => res.json())
@@ -35,7 +20,7 @@ const Home = () => {
     return (
         <div>
             {
-                featuredCars.length && reviews.length
+                reviews.length
                     ?
                     <>
                         <Navigation></Navigation>
@@ -44,10 +29,6 @@ const Home = () => {
                         <UsedCars></UsedCars>
                         <SellCar></SellCar>
                         <Container fluid>
-
-                            {/* <FeaturedCars
-                                featuredCars={featuredCars}
-                            ></FeaturedCars> */}
                             <Reviews
                                 reviews={reviews}
                             ></Reviews>
